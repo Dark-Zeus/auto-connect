afd88739a604dc75a145fa2df5281ebd8856d713
JaithLomitha2002 <djaithlomitha2002@gmail.com>
2025-06-26 21:05:54 +0530

    updated page width to w-full

--- auto-connect-frontend/src/pages/SellVehiclePage.jsx
+++ auto-connect-frontend/src/pages/SellVehiclePage.jsx
@@ -2,7 +2,7 @@ import SellVehicleForm from "@components/SellVehicleForm";
 
 const SellVehiclePage = () => {
   return (
-    <div className="tw:min-h-screen tw:bg-gradient-to-br tw:from-slate-100 tw:to-blue-50 tw:py-8 tw:px-4 tw:overflow-auto">
+    <div className="tw:min-h-screen tw:w-full tw:bg-gradient-to-br tw:from-slate-100 tw:to-blue-50 tw:py-8 tw:px-4 tw:overflow-auto">
       <div className="tw:w-full sm:tw:w-11/12 md:tw:w-4/5 lg:tw:w-[70%] tw:mx-auto">
         <SellVehicleForm />
       </div>
