27850746feeb974f6e1f27bda2b92b5ad1aff18b
JaithLomitha2002 <djaithlomitha2002@gmail.com>
2025-08-08 00:44:22 +0530

    Add listVehicle model schema for vehicle listings

---
+++ auto-connect-backend/models/listVehicle.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const listVehicleSchema = new mongoose.Schema({
+    userId: { type: String, required: true }, // Seller's user ID
+    name: { type: String, required: true }, // Seller's name
+    email: { type: String, required: true }, // Seller's email
+    mobile: { type: String, required: true },
+    district: { type: String, required: true },
+    city: String,
+    vehicleType: { type: String, required: true },
+    condition: { type: String, required: true },
+    make: { type: String, required: true },
+    model: { type: String, required: true },
+    year: { type: Number, required: true },
+    price: Number,
+    ongoingLease: Boolean,
+    transmission: { type: String, required: true },
+    fuelType: { type: String, required: true },
+    engineCapacity: Number,
+    mileage: { type: Number, required: true },
+    description: String,
+    registrationNumber: { type: String, required: true },
+    photos: [String],
+    views: { type: Number, default: 0 }, // <--- important
+    }, { timestamps: true });
+
+export default mongoose.model('ListVehicle', listVehicleSchema);
