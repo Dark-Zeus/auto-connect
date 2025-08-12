26c1e92d0c2625bf6441ce667fc996021db6129b
JaithLomitha2002 <djaithlomitha2002@gmail.com>
2025-08-13 00:05:22 +0530

    Add updateVehicleAd route to enable vehicle ad updates

--- auto-connect-backend/routes/listVehicle.route.js
+++ auto-connect-backend/routes/listVehicle.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createVehicleAd, getMyVehicleAds, getVehicleAdById } from "../controllers/listVehicle.controller.js";
+import { createVehicleAd, getMyVehicleAds, getVehicleAdById, updateVehicleAd } from "../controllers/listVehicle.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -12,5 +12,7 @@ router.post("/", createVehicleAd);
 router.get("/my", getMyVehicleAds);
 
 router.get("/:id", getVehicleAdById);
+
+router.patch("/:id", updateVehicleAd);
 
 export default router;
