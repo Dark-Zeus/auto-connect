a331bd508f1c380fbedd95c3cd3890204d0a4c95
JaithLomitha2002 <djaithlomitha2002@gmail.com>
2025-08-16 23:42:06 +0530

    Add filter route to buyVehicle API for advanced vehicle filtering

--- auto-connect-backend/routes/buyVehicle.route.js
+++ auto-connect-backend/routes/buyVehicle.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAvailableVehicles, getVehicleById, saveAd, getSavedAds, unsaveAd } from "../controllers/buyVehicle.controller.js";
+import { getAvailableVehicles, getVehicleById, saveAd, getSavedAds, unsaveAd, filterVehicles } from "../controllers/buyVehicle.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.get("/saved", getSavedAds);
 router.get("/:id", getVehicleById);
 router.post("/save", saveAd);
 router.post("/unsave", unsaveAd);
+router.post("/filter", filterVehicles);
 
 export default router;
