c231ed704918583e14b302fb2686a419eb971f44
JaithLomitha2002 <djaithlomitha2002@gmail.com>
2025-08-16 16:37:32 +0530

    Update saveListedVehicle model schema to remove unnecessary fields

--- auto-connect-backend/models/saveListedVehicle.model.js
+++ auto-connect-backend/models/saveListedVehicle.model.js
@@ -2,16 +2,8 @@ import mongoose from "mongoose";
 
 const saveListedVehicleSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  vehicleId: { type: mongoose.Schema.Types.ObjectId, ref: "ListVehicle", required: true },
-  make: String,
-  model: String,
-  year: Number,
-  city: String,
-  district: String,
-  price: Number,
-  mileage: Number,
-  fuelType: String,
-  postedDate: Date,
+  vehicleId: { type: mongoose.Schema.Types.ObjectId, ref: "ListVehicle", required: true }
+  // Remove make, model, year, city, district, price, mileage, fuelType, postedDate
 }, { timestamps: true });
 
 export default mongoose.model("SavedAd", saveListedVehicleSchema);
