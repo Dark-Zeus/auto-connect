f5facde4e41c4b8437cf0c21ed0cc12a5e745721
JaithLomitha2002 <djaithlomitha2002@gmail.com>
2025-08-17 00:46:46 +0530

    Add reportAd and checkIfReported routes to buyVehicle API

--- auto-connect-backend/routes/buyVehicle.route.js
+++ auto-connect-backend/routes/buyVehicle.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAvailableVehicles, getVehicleById, saveAd, getSavedAds, unsaveAd, filterVehicles } from "../controllers/buyVehicle.controller.js";
+import { getAvailableVehicles, getVehicleById, saveAd, getSavedAds, unsaveAd, filterVehicles, reportAd, checkIfReported } from "../controllers/buyVehicle.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -12,5 +12,7 @@ router.get("/:id", getVehicleById);
 router.post("/save", saveAd);
 router.post("/unsave", unsaveAd);
 router.post("/filter", filterVehicles);
+router.get("/check-reported", checkIfReported);
+router.post("/report", reportAd);
 
 export default router;
