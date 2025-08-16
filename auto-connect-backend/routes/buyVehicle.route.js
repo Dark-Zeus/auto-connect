31b624a3f1efd9ae52031e25035f457ac8b45a11
JaithLomitha2002 <djaithlomitha2002@gmail.com>
2025-08-16 12:51:53 +0530

    Add routes for saving and retrieving user ads

--- auto-connect-backend/routes/buyVehicle.route.js
+++ auto-connect-backend/routes/buyVehicle.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAvailableVehicles, getVehicleById } from "../controllers/buyVehicle.controller.js";
+import { getAvailableVehicles, getVehicleById, saveAd, getSavedAds } from "../controllers/buyVehicle.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -7,6 +7,8 @@ const router = express.Router();
 router.use(protect);
 
 router.get("/", getAvailableVehicles);
+router.get("/saved", getSavedAds);
 router.get("/:id", getVehicleById);
+router.post("/save", saveAd);
 
 export default router;
